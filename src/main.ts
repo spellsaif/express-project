e9cf20ab106bec7a61cebba8436467d22ac6d5fe
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-08-31 10:30:33 +0000

    feat:build server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import createServer from './utils/create-server';
+
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+async function main() {
+  const app = createServer();
+
+  await app.listen(SERVER_PORT, () => {
+    console.log(`Listening at http://localhost:${SERVER_PORT} `);
+  });
+}
+
+main();
---
+++ src/utils/create-server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+function createServer() {
+  const app = express();
+
+  return app;
+}
+
+export default createServer;
